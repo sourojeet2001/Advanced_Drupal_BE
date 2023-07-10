487e0515b1ac4d32a12334ec2962a1e2b87918bc
sourojeet2001 <sourojeet.paul@innoraft.com>
2023-07-10 13:54:51 +0530

    Updated the FirstController with comments

--- web/modules/custom/mymodule/src/Controller/FirstController.php
+++ web/modules/custom/mymodule/src/Controller/FirstController.php
@@ -12,28 +12,48 @@ use Drupal\Core\Cache\Cache;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\user\UserInterface;
 
-class FirstController extends ControllerBase {
+/**
+ * This class controls all the route redirection by specifying different functions.
+ */
+class FirstController extends ControllerBase {  
+  /**
+   * This function is used to render a simple page using a Hello message.
+   *
+   */
   public function simpleContent() {
     return [
       '#type' => 'markup',
       '#markup' => t(string: 'Hello Sourojeet'),
     ];
   }
-
-  public function variableContent($name1, $name2) {
+  
+  /**
+   * This fucntion is used to display dynamic content as per query parameteres.
+   *
+   *  @param  string $name1
+   *    Takes a name as query parameter.
+   *  @param  string $name2
+   *    Takes another name as query parameter.
+   */
+  public function variableContent(string $name1, string $name2) {
     return [
       '#type' => 'markup',
       '#markup' => t(string: 'Hello @name1 and @name2',
         args: ['@name1' => $name1, '@name2' => $name2]),
     ];
   }
-
-  
-  // public $user = \Drupal::currentUser();
+    
+  /**
+   * This function is used to display a hello message with the username of the 
+   * currently logged in user by using a core service of Drupal.
+   *
+   */
   public function hello() {
+    // Fetching the current user id.
     $user = \Drupal::currentUser()->id();
     if($user) {
       $cacheTags = ['user:' . \Drupal::currentUser()->id()];
+      // Rendering back the hello message with currently logged in user's name.
       return [
         '#type' =>'markup',
         '#cache' => [
@@ -42,6 +62,7 @@ class FirstController extends ControllerBase {
         '#markup' => t(string: 'Hello ' . \Drupal::currentUser()->getAccountName()),
       ];
     }
+    // For anonymous user just shows a "Hello User" message.
     else {
       return [
         '#type' =>'markup',
@@ -49,7 +70,11 @@ class FirstController extends ControllerBase {
       ];
     }
   }
-
+  
+  /**
+   * This is a function just for testing purpose.
+   *
+   */
   public function test() {
     \Drupal::messenger()->addMessage(t("Hello Sourojeet Test Successful"));
   }
